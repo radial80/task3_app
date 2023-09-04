9d43308133b60e4c7e5c2998f7a87c49a9513b6f
Recep Uyduran <recepuyduran-1@hotmail.com>
2023-09-04 19:20:53 +0300

    task3_app Done

---
+++ task3_app/TimerViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  task3_app
+//
+//  Created by Recep Uyduran on 31.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var pickerView: UIPickerView!
+    @IBOutlet weak var timerLabel: UILabel!
+    @IBOutlet weak var playPauseButton: UIButton!
+    @IBOutlet weak var setResetButton: UIButton!
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
